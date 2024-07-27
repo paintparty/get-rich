323a11074e0b219a77093d89f16f282393e750a1
Jeremiah Coyle <jcoyle@gmail.com>
2024-07-26 22:43:56 -0400

    Bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.github.paintparty/get-rich "0.1.0"
+(defproject io.github.paintparty/get-rich "0.2.0"
   :description "Rich text console printing for Clojure(Script)"
   :url "https://github.com/paintparty/fireworks"
   :license {:name "MIT"}
